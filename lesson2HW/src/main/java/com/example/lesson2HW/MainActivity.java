0148f839d475944f1143c8303eedb3914d57aa56
Keos99 <keos99@bk.ru>
2019-01-21 10:21:05 +0300

    Completed the first task. Added text tracking in EditText, the change is displayed immediately in TextViev.

--- lesson2HW/src/main/java/com/example/lesson2HW/MainActivity.java
+++ lesson2HW/src/main/java/com/example/lesson2HW/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.lesson2hw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.jakewharton.rxbinding.widget.RxTextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.edittext);
+        react();
+    }
+
+    public void react(){
+        RxTextView.textChanges(editText)
+                .subscribe(charSequence -> {
+                    textView.setText(charSequence);
+                });
     }
 }
