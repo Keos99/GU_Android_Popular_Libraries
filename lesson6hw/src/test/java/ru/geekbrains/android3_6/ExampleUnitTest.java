594ce4ad3a8fd9092af3bdc34d8675b745376faf
Keos99 <keos99@bk.ru>
2019-02-07 10:10:57 +0300

    Add code for HW6.

--- lesson6hw/src/test/java/ru/geekbrains/android3_6/ExampleUnitTest.java
+++ lesson6hw/src/test/java/ru/geekbrains/android3_6/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.lesson6hw;
+package ru.geekbrains.android3_6;
 
 import org.junit.Test;
 
@@ -9,9 +9,11 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class ExampleUnitTest
+{
     @Test
-    public void addition_isCorrect() {
+    public void addition_isCorrect() throws Exception
+    {
         assertEquals(4, 2 + 2);
     }
 }
